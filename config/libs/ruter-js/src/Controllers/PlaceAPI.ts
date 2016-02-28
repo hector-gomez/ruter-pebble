253e4fa211aeac5c1d2ee8afcf6a0e53b25a27c3
Héctor Gómez <hectorgomeziglesias@yahoo.es>
2016-02-28 16:52:04 +0100

    Created first sample TypeScript files to help implement the build scripts

---
+++ config/libs/ruter-js/src/Controllers/PlaceAPI.ts
@@ -0,0 +1,8 @@
+export class PlaceAPI {
+
+    static findPlace(name: string) {
+        //TODO Implement this method correctly
+        return "Could not find " + name;
+    }
+
+}
---
+++ config/libs/ruter-js/src/Ruter.ts
@@ -0,0 +1,10 @@
+/**
+ * Library with utilities to work with the Ruter Reise API
+ */
+
+import {PlaceAPI} from "./Controllers/PlaceAPI";
+
+export = {
+    places: PlaceAPI,
+    version: "0.0.1"
+};

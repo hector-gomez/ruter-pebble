76be795da5011e4348962c78aaf7242883520dd6
Héctor Gómez <hectorgomeziglesias@yahoo.es>
2016-04-10 17:50:36 +0200

    PlaceAPI.findPlace now returns a Promise containing an object that complies to PlaceInterface

--- config/libs/ruter-js/src/Controllers/PlaceAPI.ts
+++ config/libs/ruter-js/src/Controllers/PlaceAPI.ts
@@ -1,8 +1,22 @@
 export class PlaceAPI {
 
-    static findPlace(name: string) {
-        //TODO Implement this method correctly
-        return "Could not find " + name;
+    static findPlace(name: string): Promise<PlaceInterface> {
+        return new Promise<PlaceInterface>((resolve, reject) => {
+            // Perform some basic validation
+            if (name.length === 0) {
+                reject(new Error('The name of the place must not be empty'));
+            }
+
+            //TODO Query the Ruter API to search the place
+            let place = {
+                ID: 123,
+                Name: "Jernbanetorget",
+                District: "Oslo",
+                Zone: "1",
+                PlaceType: "Stop"
+            };
+            resolve(place);
+        });
     }
 
 }

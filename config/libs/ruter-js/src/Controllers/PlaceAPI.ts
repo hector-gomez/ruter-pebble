753fd0b2aa94f08fcca8581732a87706f8e1c09f
Héctor Gómez <hectorgomeziglesias@yahoo.es>
2016-04-16 19:01:54 +0200

    Use a module instead of a class for PlaceAPI

--- config/libs/ruter-js/src/Controllers/PlaceAPI.ts
+++ config/libs/ruter-js/src/Controllers/PlaceAPI.ts
@@ -1,22 +1,20 @@
-export class PlaceAPI {
+import * as Api from '../Utils/ApiConnector';
 
-    static findPlace(name: string): Promise<PlaceInterface> {
-        return new Promise<PlaceInterface>((resolve, reject) => {
-            // Perform some basic validation
-            if (name.length === 0) {
-                reject(new Error('The name of the place must not be empty'));
-            }
+export function findPlace(name: string): Promise<PlaceInterface> {
+    return new Promise<PlaceInterface>((resolve, reject) => {
+        // Perform some basic validation
+        if (name.length === 0) {
+            reject(new Error('The name of the place must not be empty'));
+        }
 
-            //TODO Query the Ruter API to search the place
-            let place = {
-                ID: 123,
-                Name: "Jernbanetorget",
-                District: "Oslo",
-                Zone: "1",
-                PlaceType: "Stop"
-            };
-            resolve(place);
-        });
-    }
-
+        //TODO Query the Ruter API to search the place
+        let place = {
+            ID: 123,
+            Name: "Jernbanetorget",
+            District: "Oslo",
+            Zone: "1",
+            PlaceType: "Stop"
+        };
+        resolve(place);
+    });
 }
--- config/libs/ruter-js/src/Ruter.ts
+++ config/libs/ruter-js/src/Ruter.ts
@@ -2,7 +2,7 @@
  * Library with utilities to work with the Ruter Reise API
  */
 
-import {PlaceAPI} from './Controllers/PlaceAPI';
+import * as PlaceAPI from './Controllers/PlaceAPI';
 
 export const version = '0.0.1';
 export let places = PlaceAPI;

2463bade9eb1c277fd199dd2b87e9e313d3b8e42
Héctor Gómez <hectorgomeziglesias@yahoo.es>
2016-04-18 20:01:54 +0200

    Show the search results

--- config/add_stop.js
+++ config/add_stop.js
@@ -3,18 +3,58 @@ document.forms['search-form'].onsubmit = function onSubmitSearch(event) {
     var stopName = event.target.getElementsByClassName('item-input')[0].value;
 
     // Perform the search using the SDK
-    Ruter.places.findPlace(stopName).then(onSearchSuccess, onSearchError);
+    Ruter.findPlace(stopName, 'Stop').then(onSearchSuccess, onSearchError);
 
     // Cancel the form submission
     event.stopPropagation();
     return false;
 };
 
-function onSearchSuccess(data) {
-    //TODO Implement properly
-    console.log('This is what we found', data);
+/**
+ * Callback invoked after the server has responded to the search query.
+ *
+ * @param resultSet Array of places (see PlaceInterface.ts in the Ruter SDK)
+ */
+function onSearchSuccess(resultSet) {
+    var resultsNode = document.getElementById('search-results');
+
+    // Clean the previous results, if any
+    while (resultsNode.firstChild) {
+        resultsNode.removeChild(resultsNode.firstChild);
+    }
+
+    // Populate the results
+    resultSet.forEach(function populateItem(item) {
+        resultsNode.appendChild(createListItem(item.Name, item.ID));
+    });
+
+    // Update the styles using Slate
+    Zepto('.item-radio').itemRadio();
 }
 
 function onSearchError(error) {
     console.error(error);
+}
+
+/**
+ * Helper function that creates a new DOM element for a list item, ready to be
+ * added to the search results.
+ *
+ * @param {string} placeName Name of the place.
+ * @param {string} placeId Id of the place.
+ * @returns {Element} The DOM element.
+ */
+function createListItem(placeName, placeId) {
+    var labelNode = document.createElement('label');
+    labelNode.className = 'item';
+    labelNode.textContent = placeName;
+
+    var radioNode = document.createElement('input');
+    radioNode.className = 'item-radio';
+    radioNode.name = 'place-selection';
+    radioNode.type = 'radio';
+    radioNode.value = placeId;
+    labelNode.appendChild(radioNode);
+
+    return labelNode;
 }
